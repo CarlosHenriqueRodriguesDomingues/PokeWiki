e598ec6b02c82aaa64870a3563ec2e439db2de27
Eduardo <eduardopaivadias@gmail.com>
2023-07-11 01:33:26 -0300

    Obter objeto pokemons

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../components/Card";
+import PokemonService from "../services/PokemonService";
 
 function Home() {
+  const [listPokemon, setListPokemon] = useState([]);
+
+  async function fetchDataOptions(type) {
+    const list = await PokemonService.getPokemons(type);
+    setListPokemon(list);
+  }
+
+  useEffect(() => {
+    fetchDataOptions("Normal");
+  }, []);
+
   return (
     <div>
       <Card
